601be039502c57aeb522fb006cbe64a2a7fb580d
abdulla fareez <abdulla.fareez@pictureworks.com.au>
2022-05-09 16:07:07 +0500

    controller clean-up

--- app/Http/Controllers/Api/LocationController.php
+++ app/Http/Controllers/Api/LocationController.php
@@ -2,14 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Events\ReportAlerts;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LocationRequest;
-use App\Models\Location;
 use App\Services\LocationService;
-use App\Services\WeatherService;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 
 class LocationController extends Controller
 {
